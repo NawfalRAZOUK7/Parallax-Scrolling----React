f2c57df393c231f7242bb86e5ef1ec66f308ed3a
Nawfal RAZOUK <nawfalrazouk7@gmail.com>
2023-02-14 03:26:36 +0100

    First step to React

---
+++ src/textBlock.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function TextBlock() {
+    return (
+        <div id="textblock">
+            <div id="textblock-container">
+                <h1 id="textblock-title">Why Pheonix?</h1>
+                <p id="textblock-content">
+                    My name is Nawfal.<br /><br />
+                    I believe that the Phoenix is a powerful symbol of transformation and rebirth. In many cultures, the Phoenix is associated with the sun and is believed to live for hundreds or even thousands of years before bursting into flames and being reborn from its own ashes. This imagery represents the idea of rising from the ashes of one's past to emerge stronger and more resilient than before.<br /><br />
+                    I chose to call myself Phoenix because I have faced difficult challenges and obstacles in my life. However, I have always strived to overcome them and emerge stronger. I see myself as a person who can learn from my past experiences and transform them into something positive, just as the Phoenix rises from the ashes.<br /><br />
+                </p>
+            </div>
+            <footer id='textblock-footer'>Demo Created With 🧡 By&nbsp;<a id="textblock-devsense" href="https://www.nawfalrazouk.live">Razouk Nawfal</a></footer>
+        </div>
+    );
+}
+
+export default TextBlock;
